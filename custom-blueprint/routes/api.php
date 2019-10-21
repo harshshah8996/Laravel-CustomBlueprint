07ab48252228963c46f9fe3b96a61df0beedf191
Harsh Shah <harsh_shah@gridle.io>
2019-10-21 14:57:24 +0530

    [Add] : Auth route

--- custom-blueprint/routes/api.php
+++ custom-blueprint/routes/api.php
@@ -1,6 +1,8 @@
+
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/sign-up', 'AuthController@signUp');
+    Route::post('/login', 'AuthController@login');
+    
 });
