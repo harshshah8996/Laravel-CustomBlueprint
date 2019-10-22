9f67c7549ea9d0834ec02ae6276b2ea52a9a83cb
Harsh Shah <harsh_shah@gridle.io>
2019-10-22 15:38:10 +0530

    [Implement] : Admin middlerware

---
+++ custom-blueprint/app/Http/Middleware/Admin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+use Illuminate\Support\Facades\Auth;
+
+use Closure;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::user() && Auth::user()['role_id'] != 2) {
+            return $next($request);
+        }
+        return response()->json([
+            'errors' => 'Error',
+            'message' => 'Unauthorized',
+        ], 401);
+    }
+}
