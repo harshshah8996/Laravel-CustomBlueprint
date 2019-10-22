1f8df6491ad123bd5731241b006187596fee4ebd
Harsh Shah <harsh_shah@gridle.io>
2019-10-22 15:35:48 +0530

    [Update] : Constant file

--- custom-blueprint/config/constants.php
+++ custom-blueprint/config/constants.php
@@ -1,11 +1,6 @@
 <?php
 
 return [
-    'GENDER' => [
-        'MALE' => 'M',
-        'FEMALE' => 'F'
-    ],
-
     'MASTER_ADMIN_ROLE_ID' => 1,
-    'STUDENT_ROLE_ID' => 2
+    'Student_ROLE_ID' => 2
 ];
